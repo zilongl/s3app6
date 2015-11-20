607d4cb34341a8fe0497ac1a9cc7a98a85a20b31
Zi Long <zilong0206@hotmail.com>
2015-11-20 02:11:44 -0500

    Arrange SharedMemoryUtilities

--- SharedMemoryUtilities/SharedMemoryCreateBuffer.java
+++ SharedMemoryUtilities/SharedMemoryCreateBuffer.java
@@ -15,26 +15,28 @@ public class SharedMemoryCreateBuffer
       SharedMemory sm = new SharedMemory();
 
       System.out.println("Allocation de la memoire");
-	  int handle = sm.alloc(5,5);
+	   int handle = sm.alloc(5,5);
 
-	  sm.displaySharedMemory(handle);
+	   sm.displaySharedMemory(handle);
 
-	  InputStreamReader converter = new InputStreamReader (System.in);
-	  BufferedReader		in = new BufferedReader (converter);
+	   InputStreamReader converter = new InputStreamReader (System.in);
+	   BufferedReader		in = new BufferedReader (converter);
       String anyKey = null;
-	  try {
-	    anyKey = in.readLine();
-	  } catch( Exception e ) {
-	       System.out.println("Erreur de lecture de string");
-	  }
+	   try {
+	      anyKey = in.readLine();
+	   } catch( Exception e ) {
+	      System.out.println("Erreur de lecture de string");
+	   }
 
       System.out.println("On doit liberer la memoire partage...");
       sm.free(handle);
     }
+    
     catch(Throwable t)
     {
       System.out.println(t);
     }
+    
     System.out.println("Exiting Shared Memory Example");
   }
 }
